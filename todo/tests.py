021a59b8ce2597b25701d48a9d42febe4e167064
Balaji D R <drbalaji97@gmail.com>
2018-05-09 15:05:14 +0530

    written test cases for the create user

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import User
+from todo.functions import core as core
+
+class TestUserUpdates(TestCase):
+    def setUp(self):
+        pass
+
+    def test_create_user(self):
+        user = User()
+        user = core.createUser("deeaarbee","spiderman","drbalaji97@gmail.com",2015103506)
+        self.assertEqual(user.username,"deeaarbee")
+        # self.assertEqual(user.password,"spiderman")
+        self.assertEqual(user.email,"drbalaji97@gmail.com")
+        self.assertEqual(user.rollnumber, 2015103506)
+
